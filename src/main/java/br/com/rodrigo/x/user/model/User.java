cbc66ff9aa8509bf42a058d50af4f31c3b14c58c
Rodrigo Barbosa <rodrigobs8157@gmail.com>
2024-07-31 01:31:19 -0300

    feat: Implementação do @Data do lombok na entidade User

--- src/main/java/br/com/rodrigo/x/user/model/User.java
+++ src/main/java/br/com/rodrigo/x/user/model/User.java
@@ -15,9 +15,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "tb_user")
@@ -50,68 +52,4 @@ public class User {
 	
 	@Column(name = "password", nullable = false)
 	private String password;
-
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public Role getRole() {
-		return role;
-	}
-
-	public void setRole(Role role) {
-		this.role = role;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public LocalDate getDateOfBirth() {
-		return dateOfBirth;
-	}
-
-	public void setDateOfBirth(LocalDate dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-
-	public LocalDateTime getDateOfCreation() {
-		return dateOfCreation;
-	}
-
-	public void setDateOfCreation(LocalDateTime dateOfCreation) {
-		this.dateOfCreation = dateOfCreation;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
